dbd15dec4dcf7b4895427d13480cba0669954e43
Zainab Azzam <zainab_azzam@yahoo.com>
2020-05-15 23:37:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,21 +9,30 @@ function leashDog (dogName, dogBreed)
 {
   
   console.log(`Leash ${dogName} the ${dogBreed}`);
+  let ret=`Leash ${dogName} the ${dogBreed}`
+  return ret;
 }
 function walkToPark (dogName, dogBreed)
 {
   
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  let ret=`Walk to the park with ${dogName} the ${dogBreed}`
+  return ret;
 }
 function throwFrisbee(dogName, dogBreed)
 {
   
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  let ret=`Throw the frisbee for ${dogName} the ${dogBreed}`
+  return ret;
+  
 }
 function walkHome(dogName, dogBreed)
 {
   
   console.log(`Walk home with ${dogName} the ${dogBreed}`);
+   let ret=`Throw the frisbee for ${dogName} the ${dogBreed}`
+  return ret;
 }
 function unleashDog (dogName, dogBreed)
 {
