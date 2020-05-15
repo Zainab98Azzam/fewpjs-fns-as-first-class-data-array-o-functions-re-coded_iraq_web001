5d1162e76fbce2e9a53e0c13b13e2bdd177d8c00
Zainab Azzam <zainab_azzam@yahoo.com>
2020-05-15 23:27:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-function wakeDog(dogName, dogBreed){
+function wakeDog(dogName, dogBreed)
+{
   
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
-function leashDog (dogName, dogBreed){
+function leashDog (dogName, dogBreed)
+{
   
   console.log(`Leash ${dogName} the ${dogBreed}`);
 }
-function walkToPark (dogName, dogBreed){
+function walkToPark (dogName, dogBreed)
+{
   
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
 }
-function throwFrisbee(dogName, dogBreed){
+function throwFrisbee(dogName, dogBreed)
+{
   
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
 }
-function walkHome(dogName, dogBreed){
+function walkHome(dogName, dogBreed)
+{
   
   console.log(`Walk home with ${dogName} the ${dogBreed}`);
 }
-function unleashDog (dogName, dogBreed){
+function unleashDog (dogName, dogBreed)
+{
   
   console.log(`Unleash ${dogName} the ${dogBreed}`);
 }
 
 let routine =[wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
 
-function exerciseDog (dogName, dogBreed){
+function exerciseDog (dogName, dogBreed)
+{
   let routine =[wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
  let a=routine.wakeDog(dogName, dogBreed)
  let b=routine.leashDog(dogName, dogBreed)
